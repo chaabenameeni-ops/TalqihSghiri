75fb6b0adcdebc659fe95181b0142c62daa8b3e6
chaabenameeni-ops <chaabenameeni@gmail.com>
2026-05-06 16:20:58 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
-$conn = mysqli_connect(
-    getenv('MYSQLHOST'),
-    getenv('MYSQLUSER'),
-    getenv('MYSQLPASSWORD'),
-    getenv('MYSQLDATABASE'),
-    getenv('MYSQLPORT')
-);
+$host = getenv('MYSQLHOST') ?: 'localhost';
+$user = getenv('MYSQLUSER') ?: 'root';
+$pass = getenv('MYSQLPASSWORD') ?: '';
+$db   = getenv('MYSQLDATABASE') ?: 'vaccins';
+$port = getenv('MYSQLPORT') ?: 3306;
+
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
